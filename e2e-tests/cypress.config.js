9fbb5dd73e22b5880da986a0ba07c3492f7a2aa4
marsheuss <marcelinhond@hotmail.com>
2026-05-21 16:08:55 -0300

    chore: reorganiza e2e-tests com tests/ e screenshots/ no padrao da turma

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   allowCypressEnv: false,
+  screenshotsFolder: "screenshots",
+  videosFolder: "videos",
 
   e2e: {
+    specPattern: "tests/**/*.cy.{js,jsx}",
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
