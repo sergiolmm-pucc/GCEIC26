9a767e815c44372dd4f1e286004b31b63f3d8f74
Douglas Alves <douglasmonttozo@hotmail.com.br>
2026-05-26 12:46:04 -0300

    fix: atualiza nome do botão para G16 - MarkUp Calc

--- app/index.js
+++ app/index.js
@@ -35,7 +35,7 @@ const equipes = [
   { numero: 13, nome: 'Equipe-13',   rota: '/equipe-13' },
   { numero: 14, nome: 'Equipe-14',   rota: '/equipe-14' },
   { numero: 15, nome: 'Equipe-15',   rota: '/equipe-15' },
-  { numero: 16, nome: 'MarkUp Calc', rota: '/grupo-16' },
+  { numero: 16, nome: 'G16 - MarkUp Calc', rota: '/grupo-16' },
   { numero: 17, nome: 'Equipe-17',   rota: '/equipe-17' },
   { numero: 18, nome: 'Equipe-18',   rota: '/equipe-18' },
   { numero: 19, nome: 'Equipe-19',   rota: '/equipe-19' },
