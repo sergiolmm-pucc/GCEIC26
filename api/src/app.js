ba0ef6ba9c5914cdcc1588db6847402724ac4e53
MatheusCronograma <164568874+MatheusCronograma@users.noreply.github.com>
2026-05-27 10:08:34 -0300

    Revert "feat - integração grupo17"

--- api/src/app.js
+++ api/src/app.js
@@ -8,45 +8,43 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-const nfvendaRouter = require('./equipe-17/nfvendaRoutes');
+const pricingRouter = require('./equipe-14/pricingRoutes');
 
 // checa se api no ar
 app.get('/health', (req, res) => {
-  res.json({ status: 'ok', timestamp: new Date().toISOString() , by:'SLMM28', turma:'101'});
+  res.json({ status: 'ok', timestamp: new Date().toISOString() , by:'SLMM-33', turma:'101'});
 });
 
-app.use('/nfvenda', nfvendaRouter);
+app.use('/PBL', pricingRouter);
 
 app.get('/api/tabelas', (req, res) => {
-  const { TABELA } = require('./funcoes');
+  const { TABELA } = require('./equipe-16/funcoes');
   res.json({
     success: true,
     data: {
-      base: TABELA.BASE_CALC.faixas,
-      referencia: `${TABELA.REFERENCIA * 100}%`,
+      campos: TABELA.CAMPOS,
+      formula: TABELA.FORMULA,
     },
   });
+
 });
 
 // POST /api/calcular
 app.post('/api/calcular', (req, res) => {
   try {
-    const { calcular } = require('./funcoes');
+    const { calcular } = require('./equipe-16/funcoes');
     const dados = req.body;
 
     if (!dados || typeof dados !== 'object') {
-      return res.status(400).json({ error: 'Corpo da requisição inválido' });
+      return res.status(400).json({ success: false, error: 'Corpo da requisição inválido' });
     }
-
+    
     const resultado = calcular(dados);
-    console.log(resultado);
     return res.status(200).json({ success: true, data: resultado });
   } catch (err) {
-    console.log(err.message);
     return res.status(400).json({ success: false, error: err.message });
   }
 });
 
-module.exports = app;
-
+module.exports = app
 
