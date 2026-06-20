0b4a87f30261c4aafd9a50c481ecdf1e34a61d85
Nicolas Mitjans Nunes <nickmitjansnunes@gmail.com>
2026-06-20 14:33:40 -0300

    alteração no arquivo vite

--- app/vite.config.equipe7.mjs
+++ app/vite.config.equipe7.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Veio do seu arquivo original
+import react from '@vitejs/plugin-react'; 
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   root: path.resolve(__dirname, 'views/equipe-7'), // Onde estão seus arquivos fontes
   base: '/equipe-7/', 
+  
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', 
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+
   build: {
-    // 📍 ALTERADO: Agora a pasta dist vai nascer DIRETO dentro da sua pasta equipe-7!
+    // Agora a pasta dist vai nascer DIRETO dentro da sua pasta equipe-7!
     outDir: path.resolve(__dirname, 'views/equipe-7/dist'), 
     emptyOutDir: true, // Limpa apenas a SUA nova pasta dist antes de gerar os arquivos
   },
